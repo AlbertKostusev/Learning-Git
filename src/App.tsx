cd85c9b0d092bda9503d8743d0ff8a193c8ef235
Albert Kostusev <albert.kostusev@veriff.net>
2020-11-12 15:26:49 +0200

    Implement logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 
 // This is a long doc. comment, should be commited seperately
 function App() {
+  const [state, setState] = React.useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <button style={{marginRight:20}}>Add 1</button>
-          <button>Remove 1</button>
+          <button style={{marginRight:20}} onClick={() => setState(state +1)}>Add 1</button>
+          <button onClick={() => setState(state -1)}>Remove 1</button>
         </div>
         <h2>Count: </h2>
         <button></button>
